4ec707a578cce3eca72cfcaf78a897c750ce9668
Pandacy <anuwat@live.nl>
2020-03-31 14:42:57 +0200

    Changes and tests added to project

---
+++ TramVerdeelSysteem/Data/Authorisation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Data
+{
+    public class Authorisation
+    {
+        private readonly ConnectionClass _connect;
+        public Authorisation(ConnectionClass connect)
+        {
+            _connect = connect;
+        }
+
+        public string GetPassword(string Name)
+        {
+            string Password = "";
+            try
+            {
+                _connect.Con.Open();
+
+                string query = "SELECT `Password` FROM `User` WHERE `Name`=@Name";
+                MySqlCommand cmd = new MySqlCommand(query, _connect.Con);
+                cmd.Parameters.AddWithValue("@Name", Name);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Password = dataReader["Password"] + "";
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+            
+            return Password;
+        }
+
+        public void AuthorisePerson(int UserID, int UniqueKey, DateTime Date)
+        {
+            _connect.Con.Open();
+            try
+            {
+                MySqlCommand cmd = _connect.Con.CreateCommand();
+                cmd.CommandText = "INSERT INTO `authorisationlist` (`idUser`, `UniqueKey`, `Date`) VALUES (@idUser, @UniqueKey, @Date)";
+                cmd.Parameters.AddWithValue("@idUser", UserID);
+                cmd.Parameters.AddWithValue("@UniqueKey", UniqueKey);
+                cmd.Parameters.AddWithValue("@Date", Date);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+        }
+
+        public void DeauthorisePerson(int UserID)
+        {
+            _connect.Con.Open();
+            try
+            {
+                MySqlCommand cmd = _connect.Con.CreateCommand();
+                cmd.CommandText = "DELETE FROM `authorisationlist` WHERE @idUser";
+                cmd.Parameters.AddWithValue("@idUser", UserID);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+        }
+    }
+}
--- TramVerdeelSysteem/UnitTestProject1/LoginTest.cs
+++ TramVerdeelSysteem/UnitTestProject1/LoginTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Data;
+using System;
 
 namespace DataTest
 {
@@ -12,13 +13,39 @@ namespace DataTest
             //Arrange
             string username = "test";
             ConnectionClass con = new ConnectionClass("Server=studmysql01.fhict.local;Uid=dbi405544;Database=dbi405544;Pwd=SirBotler;");
-            Class1 test = new Class1(con);
+            Authorisation test = new Authorisation(con);
 
             //Act
             var output = test.GetPassword(username);
 
-            //Asserrt
+            //Assert
             Assert.AreEqual(output, "test");
+        }
+
+        [TestMethod]
+        public void TestInsertQuery()
+        {
+            //Arrange
+            ConnectionClass con = new ConnectionClass("Server=studmysql01.fhict.local;Uid=dbi405544;Database=dbi405544;Pwd=SirBotler;");
+            Authorisation test = new Authorisation(con);
+
+            //Act
+            test.AuthorisePerson(1, 1, DateTime.Now);
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestDeleteQuery()
+        {
+            //Arrange
+            ConnectionClass con = new ConnectionClass("Server=studmysql01.fhict.local;Uid=dbi405544;Database=dbi405544;Pwd=SirBotler;");
+            Authorisation test = new Authorisation(con);
+
+            //Act
+            test.DeauthorisePerson(1);
+
+            //Assert
         }
     }
 }
