e2cb4bdc9230ba08dd4ceb70411b55cda0a34c11
JoostBee <j.burggraaff@student.fontys.nl>
2020-03-31 13:48:39 +0200

    Authorizatoin class

---
+++ TramVerdeelSysteem/Logic/Interfaces/Database/IDatabaseAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+using Model.DTOs;
+
+namespace Logic.Interfaces
+{
+    public interface IDatabaseAccount
+    {
+        void Login(string username, DateTime dateTime);
+
+        void Logout(int id, DateTime dateTime);
+
+        void AddAccount(int roleID, string name, string hashedPassword);
+
+
+        void RemoveAccount(int ID);
+
+
+        AccountDTO GetUser(string username);
+
+    }
+}
---
+++ TramVerdeelSysteem/Model/DTOs/AccountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.DTOs
+{
+    public class AccountDTO
+    {
+        public string username { get; set; }
+        public string hashedPassword { get; set; }
+        public int id { get; set; }
+        public DateTime datetime { get; set; }
+    }
+}
