a7afd4b979ec5b6fb986018a16b2ed12ee81de18
Unknown <sander_nijhuis@live.nl>
2020-04-15 10:45:00 +0200

    Added Idatabasemaintenance

---
+++ TramVerdeelSysteem/Data/Interfaces/IDatabaseMaintenance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.DTOs;
+
+namespace Data.Interfaces
+{
+    public interface IDatabaseMaintenance
+    {
+        void AddCleaning();
+        void AddService();
+        void RemoveCleaning();
+        void RemoveService();
+        void IndicateCompleteCleaning(CleaningDTO cleaning);
+        void IndicateCompleteService(MaintenanceDTO service);
+        void GetServiceList();
+        void GetCleaningList();
+        void GetCleaningHistory();
+        void GetServiceHistory();
+
+
+
+    }
+}
