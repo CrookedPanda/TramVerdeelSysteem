698bbb1cc09d1647eb579c763b52b906bdc6e7c8
Pjotr Claassen <pjotr.claassen@gmail.com>
2020-04-15 09:32:38 +0200

    added cleaning class to datalayer

---
+++ TramVerdeelSysteem/Data/Cleaning.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Interfaces;
+using Model.DTOs;
+using MySql.Data.MySqlClient;
+
+namespace Data
+{
+    public class Cleaning
+    {
+        private readonly ConnectionClass _connect;
+        public Cleaning(ConnectionClass connect)
+        {
+            _connect = connect;
+        }
+
+        public Cleaning()
+        {
+            _connect = new ConnectionClass();
+        }
+    }
+}
