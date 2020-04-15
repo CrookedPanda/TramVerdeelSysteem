cb933ed4679dc2cca0a3b5318585fdac86447b1f
JoostBee <j.burggraaff@student.fontys.nl>
2020-04-15 10:31:54 +0200

    Maintenance start

---
+++ TramVerdeelSysteem/Logic/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    class Service
+    {
+        Train Train { get; set; }
+        Size ServiceSize { get; set; }
+
+        bool Priority { get; set; }
+
+        public bool CompleteService()
+        {
+
+        }
+    }
+}
---
+++ TramVerdeelSysteem/Logic/Train.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    class Train
+    {
+        public int Number { get; set; }
+        public int Line { get; set; }
+        public Status TrainStatus { get; set; }
+
+        public enum Status
+        {
+            Defect,
+            Cleaning,
+            InService,
+            InDepot
+        }
+    }
+}
