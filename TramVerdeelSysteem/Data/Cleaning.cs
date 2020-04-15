6c454c57bca065676dd0f17c106b32fa8f945144
Pjotr Claassen <pjotr.claassen@gmail.com>
2020-04-15 14:24:36 +0200

    Caught up to maintenance with cleaning

--- TramVerdeelSysteem/Data/Cleaning.cs
+++ TramVerdeelSysteem/Data/Cleaning.cs
@@ -44,9 +44,139 @@ namespace Data
             }
         }
 
-        public void RemoveCleaning()
+        private void AddCleaningHistory(CleaningDTO cleaning)
         {
+            try
+            {
+                _connect.Con.Open();
+                MySqlCommand cmd = _connect.Con.CreateCommand();
+                cmd.CommandText = "INSERT INTO `cleaninghistory` (`idTram`, `idUser`, `ServiceDate`, `Description`) VALUES (@idTram, (SELECT idUser FROM authorisationlist WHERE Name = @AuthKey), @Date, @Annotation)";
+                cmd.Parameters.AddWithValue("@idTram", cleaning.Target);
+                cmd.Parameters.AddWithValue("@AuthKey", cleaning.AuthKey);
+                cmd.Parameters.AddWithValue("@Description", cleaning.Annotation);
+                cmd.Parameters.AddWithValue("@Date", DateTime.Now);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+        }
 
+        public bool RemoveCleaning(int cleaningId)
+        {
+            try
+            {
+                _connect.Con.Open();
+                MySqlCommand cmd = _connect.Con.CreateCommand();
+                cmd.CommandText = "DELETE FROM cleaning WHERE idService = @CleanId";
+                cmd.Parameters.AddWithValue("@CleanId", cleaningId);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+            return false;
+        }
+
+        public void RemoveCleaningHistory(int cleaningId)
+        {
+            try
+            {
+                _connect.Con.Open();
+                MySqlCommand cmd = _connect.Con.CreateCommand();
+                cmd.CommandText = "DELETE FROM cleaninghistory WHERE idServiceHistory = @CleanId";
+                cmd.Parameters.AddWithValue("@CleanId", cleaningId);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+        }
+
+        public List<CleaningDTO> GetCleaningList()
+        {
+            List<CleaningDTO> cleaningList = new List<CleaningDTO>();
+            try
+            {
+                _connect.Con.Open();
+                string query = "SELECT cleaning.idTram, cleaning.Size, cleaning.Priority, cleaning.Description FROM cleaning";
+
+                MySqlCommand cmd = new MySqlCommand(query, _connect.Con);
+                var dataReader = cmd.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        CleaningDTO cleaning = new CleaningDTO
+                        {
+                            Target = dataReader.GetInt32("idTram"),
+                            Annotation = dataReader.GetString("Description")
+                        };
+                        cleaningList.Add(cleaning);
+                    }
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+            return cleaningList;
+        }
+
+        public List<CleaningDTO> GetCleaningHistory()
+        {
+            List<CleaningDTO> cleaningList = new List<CleaningDTO>();
+            try
+            {
+                _connect.Con.Open();
+                string query = "SELECT user.Name, ch.idTram, ch.ServiceDate, ch.Description FROM cleaninghistory AS ch"
+                    + " INNER JOIN user ON user.idUser = ch.idUser";
+                MySqlCommand cmd = new MySqlCommand(query, _connect.Con);
+                var dataReader = cmd.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        CleaningDTO cleaning = new CleaningDTO
+                        {
+                            Target = dataReader.GetInt32("idTram"),
+                            Annotation = dataReader.GetString("Description")
+                        };
+                    }
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _connect.Con.Close();
+            }
+            return cleaningList;
         }
     }
 }
