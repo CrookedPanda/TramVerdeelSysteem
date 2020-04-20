1387baa528d0b4326afef59c8fe679f6b519ef7a
Luuk <luukpaans2000@gmail.com>
2020-04-20 13:30:41 +0200

    conducorView bools gamkaat van int

--- TramVerdeelSysteem/Model/ViewModels/ConductorView.cs
+++ TramVerdeelSysteem/Model/ViewModels/ConductorView.cs
@@ -6,8 +6,8 @@ namespace Model.ViewModels
 {
     public class ConductorView
     {
-        public int TramNumber { get; set; }
-        public int Maintenance { get; set; }
+        public bool TramNumber { get; set; }
+        public bool Maintenance { get; set; }
         public int Cleaning { get; set; }
         public Logic.TramModel Tram { get; set; }
         public string Key { get; set; }
