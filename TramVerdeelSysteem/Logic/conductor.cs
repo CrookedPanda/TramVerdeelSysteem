62d5fd367869b0b947063b18e79e042dc8e5b1f8
Luuk <luukpaans2000@gmail.com>
2020-04-21 14:35:36 +0200

    Conductor places tram into database

---
+++ TramVerdeelSysteem/Logic/conductor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.Interfaces;
+using Logic.Interfaces.Database;
+using Model.DTOs;
+using Model.ViewModels;
+
+
+namespace Logic
+{
+    public class conductor
+    {
+        public IDatabaseMaintenance DataBase { get; set; }
+
+        public conductor(IDatabaseMaintenance cDataBase)
+        {
+            DataBase = cDataBase;
+        }
+
+        public conductor()
+        {
+            DataBase = new Data.Maintenance();
+
+        }
+
+        public void AddTramToCleaning(ConductorView Train)
+        {
+            if(Train.Cleaning == true)
+            {
+                CleaningDTO cleaning = new CleaningDTO() {TramNumber = Train.TramNumber,  Annotation = "de tram licht vol met bierblikjes van carnaval."};
+                DataBase.AddCleaning(cleaning);
+            }            
+        }
+    }
+}
