6753f00988aa52c709c36d26ee5c774cf4e5ac98
Matthijs Leander Richel <Matthijs.Richel022@gmail.com>
2020-05-13 13:50:42 +0200

    Kleine Remise Veranderingetjes

---
+++ TramVerdeelSysteem/Logic/Depot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Logic
+{
+    class Depot
+    {
+        public Depot()
+        {
+
+        }
+
+        public void AddTrain()
+        {
+
+        }
+
+        public void ClearSector(/*Track*/ track, int position)
+        {
+            track.ClearSector(position);
+        }
+
+        public void ReserveSector(/*Track*/ track, int position, Train train)
+        {
+            track.ReserveSector(position, train);
+        }
+
+        public void ChangeSectorStatus(/*Track*/ track, int position, Enum status)
+        {
+            track.ChangeSectorStatus(position, status);
+        }
+    }
+}
