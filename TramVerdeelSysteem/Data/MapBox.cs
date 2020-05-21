f66dc6fd4a80685bb56e87db8f6ef956bf851f1d
JoostBee <j.burggraaff@student.fontys.nl>
2020-05-21 14:20:26 +0200

    Logic + interface

---
+++ TramVerdeelSysteem/Data/MapBox.cs
@@ -0,0 +1,24 @@
+﻿using Model.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    class MapBox
+    {
+        public GeoFeatueDTO GeoFeatureMaker()
+        {
+            var request = new Request()
+            {
+                Id = 1,
+                Name = "MyRequest",
+                Fence = new Fence()
+                {
+                    Type = 2,
+                    Values = featureCollection
+                }
+            };
+        }
+    }
+}
---
+++ TramVerdeelSysteem/Logic/MapBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using Model.DTOs;
+using Newtonsoft.Json;
+
+namespace Logic
+{
+    class MapBox
+    {
+
+
+        private string GeoJsonHelper(GeoFeatueDTO GeoFeatue)
+        {
+            string geoJson = JsonConvert.SerializeObject(GeoFeatue);
+            return geoJson;
+
+        }
+    }
+}
