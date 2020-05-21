02c59c8f95b5c9cedcaa02080196149435c885ca
Matthijs Leander Richel <Matthijs.Richel022@gmail.com>
2020-05-21 14:29:59 +0200

    Controller & View aangemaakt

---
+++ TramVerdeelSysteem/Model/ViewModels/CleaningHistoryMasterView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModels
+{
+    public class CleaningHistoryMasterView
+    {
+        public List<CleaningHistoryView> cleanings { get; set; }
+        public CleaningHistoryView cleaning { get; set; }
+    }
+}
---
+++ TramVerdeelSysteem/Model/ViewModels/CleaningHistoryView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModels
+{
+    public class CleaningHistoryView
+    {
+        public int TramNumber { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+        public DateTime Datetime { get; set; }
+    }
+}
