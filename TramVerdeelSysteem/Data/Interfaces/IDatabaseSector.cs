43f56298ffe035b84926d67110b03e110da26368
Pandacy <anuwat@live.nl>
2020-06-24 13:35:20 +0200

    interface sector added missing methods

--- TramVerdeelSysteem/Data/Interfaces/IDatabaseSector.cs
+++ TramVerdeelSysteem/Data/Interfaces/IDatabaseSector.cs
@@ -8,6 +8,7 @@ namespace Data.Interfaces
     public interface IDatabaseSector
     {
         public bool GetSector(SectorDTO sector);
+        public bool IsSectorFree(SectorDTO sector);
         public bool SectorStatusChange(SectorStatusChangeDTO DTO);
         public bool ClearSectorWithTramNumber(SectorDTO sector);
         public bool ClearSector(SectorDTO DTO);
